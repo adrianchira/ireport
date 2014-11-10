e3dc3e640e071dfc91a25e595bcc2b0d9cf31347
Adrian Chira <adrian.chira@rocketmail.com>
2014-11-10 11:23:47 +0200

    test commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ireport::Application.config.secret_key_base = '2750e8c8563327244cc3fc7ad65d1edc639af37d5769e0d8ed09f22ba3573e5de42812acf1584267b526bfd7a6abd0ca19ef9d126754f2efeed00fbd4cbe021b'
